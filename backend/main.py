72e643656583d5565fbd5e6cbe7576cca6be649b
Chandu <chandu2006gajjala@gmail.com>
2026-06-18 20:12:25 +0530

    fixing getenv

--- backend/main.py
+++ backend/main.py
@@ -65,7 +65,7 @@ conf = ConnectionConfig(
 
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
 
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD")"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
 
     MAIL_FROM=os.getenv("MAIL_FROM"),
 
